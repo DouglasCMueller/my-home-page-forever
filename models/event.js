b2cb0c8a9389e74e7adf97d8a721c9a613fe2873
Douglas Mueller <douglasmueller6@gmail.com>
2020-03-21 16:01:32 -0400

    restoring old version

--- models/event.js
+++ models/event.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 const eventSchema = new Schema({
     title: {type: String},
     note: {type: String}
-})
+}, { collection: 'event' })
 const Event = mongoose.model("Event", eventSchema);
 
 module.exports = Event;
